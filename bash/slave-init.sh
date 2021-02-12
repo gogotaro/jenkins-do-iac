d11e18a8acb5709547d65440d7a7bd2545b5e6fb
Cabelo Doido <sorrasak.casual@gmail.com>
2021-02-12 20:46:26 +0700

    adjust slave-init.sh

--- bash/slave-init.sh
+++ bash/slave-init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+apt install httping -y
 while ! httping -qc1 http://ipv4AddressPrivate:8080/login ; do sleep 1 ; done
 slaveSecret=$(echo "$(curl -L http://jenkinsUser:jenkinsPassword@ipv4AddressPrivate:8080/computer/jenkins-agent/slave-agent.jnlp)" | sed 's/.*<application-desc main-class="hudson.remoting.jnlp.Main"><argument>//; s/<\/argument>.*//')
+echo $slaveSecret > /root/jenkinsSecret
